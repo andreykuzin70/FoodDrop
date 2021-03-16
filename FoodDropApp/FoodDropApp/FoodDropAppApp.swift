27632c6dc33d79740ebc15e8b3979b0a982db632
Andrey Kuzin <akuzin1@umd.edu>
2021-03-16 12:47:33 -0400

    App created

---
+++ FoodDropApp/FoodDropApp/FoodDropAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDropAppApp.swift
+//  FoodDropApp
+//
+//  Created by Andrey Kuzin on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodDropAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
