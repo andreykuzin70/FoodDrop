82bf03a54fbd25aa96d95a6149fa8d9aa3296905
Andrey Kuzin <akuzin1@umd.edu>
2021-03-18 11:39:31 -0400

    Food Post

---
+++ FoodDropApp/models/FoodPost.swift
@@ -0,0 +1,16 @@
+//
+//  FoodPost.swift
+//  FoodDropApp
+//
+//  Created by Andrey Kuzin on 3/18/21.
+//
+
+import Foundation
+
+class FoodPost:ObservableObject{
+    @Published var madeOn: Date
+    @Published var pickUp: Date
+    @Published var typeOfFood: String
+    @Published var 
+    
+}
