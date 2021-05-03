11131e0526bba946e2182dd23db1a57080e23a15
Natnael Mekonnen <natnaelmek@gmail.com>
2021-05-03 05:28:29 -0400

    remove xcodeproj

---
+++ FoodDropApp/FoodDropApp/Donation.swift
@@ -0,0 +1,20 @@
+//
+//  Donation.swift
+//  FoodDropApp
+//
+//  Created by Natnael Mekonnen on 5/3/21.
+//
+
+import SwiftUI
+
+struct Donation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Donation_Previews: PreviewProvider {
+    static var previews: some View {
+        Donation()
+    }
+}
